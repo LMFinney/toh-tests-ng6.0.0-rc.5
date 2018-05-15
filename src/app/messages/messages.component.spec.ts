4f484b3df0aa9acf7730ba18be0f61acab4faaf7
Lance Finney <lance.finney@oasisdigital.com>
2018-05-15 14:04:45 -0500

    Show non-shakable services

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -1,5 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
+import { MessageService } from '../message.service';
 import { MessagesComponent } from './messages.component';
 
 describe('MessagesComponent', () => {
@@ -8,9 +9,10 @@ describe('MessagesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MessagesComponent ]
+      declarations: [MessagesComponent],
+      providers: [MessageService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
